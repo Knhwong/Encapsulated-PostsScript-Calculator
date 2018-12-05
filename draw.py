444ef105ca72c65a7b0302bc1fa7ee9cbaecf55e
Kin Hei Wong <knhwong@ucdavis.edu>
2018-12-05 15:26:47 -0800

    fixed sector

--- draw.py
+++ draw.py
@@ -233,6 +233,15 @@ class sector:
         self.radius=float(draw[(index+3)])
         self.angle1=float(draw[(index+4)])
         self.angle2=float(draw[(index+5)])
+        while self.angle2 >= 360:
+            r=self.angle2//360
+            self.angle2=self.angle2-(r*360)
+        while self.angle1 >= 360:
+            r=self.angle1//360
+            self.angle1=self.angle1-(r*360)
+        while self.angle2 > 360:
+            r=angle2//360
+            angle2=angle2-(r*360)
         while self.angle2 < 0:
             self.angle2 = self.angle2 + 360
         while self.angle1 < 0:
